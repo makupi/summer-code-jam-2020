0076056feec9cc0584604b51f8fc6c787d960711
Dawid <davidblonsky88@gmail.com>
2020-08-08 02:06:50 +0200

    fix stock_api.py

--- friendly-frogs/friendly_frogs/morning/stock_api.py
+++ friendly-frogs/friendly_frogs/morning/stock_api.py
@@ -1,20 +1,9 @@
 from yahoo_fin import stock_info as si
 
-class Stock:
-
-    def __getattr__(self, item):
-        try:
-            return si.get_live_price(item)
-        except Exception:
-            return 'No data'
-
-
 def get_stocks_price(you_stocks):
     for stock in you_stocks:
-        class_object = globals()['Stock']
-        yield stock,class_object().__getattr__(stock)
+        try:
+            yield stock,si.get_live_price(stock)
+        except Exception:
+            yield stock,'No data'
 
-if __name__ == "__main__":
-    stocks = ['apple', 'microsoft', 'FELE']
-    for name,price in get_stocks_price(stocks):
-        print(name,price)
